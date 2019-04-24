69d0346eb318da39be9c6da690ff0487d7d3eebc
Astr0l <41875791+Astr0l@users.noreply.github.com>
2019-04-23 21:18:03 -0500

    Added path input

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import wget
 import sys
 import os
 
-currentPath = os.getcwd()
-
 def install(url, path, file):
     pass
+
+while True:
+    # Ask for path - default is path of script
+    currentPath = str(input("[!] Enter the path to install {Press enter to go on with Default -> path of script}: "))
+    if currentPath == " " or currentPath == "" or currentPath == None:
+        currentPath = os.getcwd()
